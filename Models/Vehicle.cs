3ca4ac3566d38b2ad350edb61f06d4a31da702fd
tom-sharp <dink521g@gmail.com>
2021-09-07 10:04:29 +0200

    ChangedModelsVehicle/Type

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -11,21 +11,24 @@ namespace Garage3.Models
 		public int Id { get; set; }
 		public string Make { get; set; }
 		public string Model { get; set; }
+
 		[DataType(DataType.DateTime)]
 		[Display(Name = "Check-in Time")]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
 		public DateTime CheckInTime { get; set; }
+
 		[DataType(DataType.DateTime)]
 		[Display(Name = "Check-out Time")]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
 		public DateTime CheckOuTime { get; set; }
+
 		public int State { get; set; }
 		public VehicleColor Color { get; set; }
         public string LicensePlate { get; set; }
 
-        public int VehicleTypeId { get; set; }
 
-		public ICollection<VehicleType> VehicleType { get; set; }
+        public int VehicleTypeId { get; set; }
+		public VehicleType VehicleType { get; set; }
 
 		public int PersonId { get; set; }
 		public Person Person { get; set; }
