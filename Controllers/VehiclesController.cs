acca4c52fc6efa4b57c77f48e4569ae0f0c54513
tom-sharp <dink521g@gmail.com>
2021-09-08 15:42:22 +0200

    ParkVehicle and TestPark Added

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Garage3.Data;
 using Garage3.Models;
+using Garage3.Models.ViewModels;
 
 namespace Garage3.Controllers
 {
@@ -162,49 +163,106 @@ namespace Garage3.Controllers
 			return _context.Vehicles.Any(e => e.Id == id);
 		}
 
+		public async Task<IActionResult> TestPark()
+		{
+			var garage = await _context.Garages.FirstOrDefaultAsync(g=> g.Size > 0);
+			if (garage == null) return View("_Msg", new MsgViewModel("Could not find any garage to check in to"));
+
+			var vehicle = await _context.Vehicles.FirstOrDefaultAsync(v => v.State == VehicleState.UnParked);
+			if (vehicle == null) return View("_Msg", new MsgViewModel("Could not find any Vehicle to check in"));
+
+			if (await ParkVehicle(garage.Id, vehicle.Id)) {
+				return View("_Msg", new MsgViewModel("Success check-in (test)"));
+			}
+			return View("_Msg", new MsgViewModel("failed check-in (test)"));
+		}
 
 
 		private async Task<bool> ParkVehicle(int garageid, int vehicleid)
 		{
 			if ((garageid <= 0) || (vehicleid <= 0)) return false;
-			var vehicle = await _context.Vehicles.FindAsync(vehicleid);
-			if (vehicle == null) return false;
+			var vehicle = _context.Vehicles.Include(v=> v.VehicleType).FirstOrDefault(v=> v.Id == vehicleid);
+			if ((vehicle == null) || (vehicle.State == VehicleState.Parked)) return false;
 			var garage = await _context.Garages.FindAsync(garageid);
 			if (garage == null) return false;
 
-			// succesful get garage & vehicle
 			// get slots for garage
+			int slotsize = garage.SlotSize;
+			var garageslots = await _context.Slots.Include(s=> s.Vehicles).Where(s => s.GarageId == garage.Id).OrderBy(s=> s.No).ToListAsync();
+			foreach (var slot in garageslots) {
+				if ((slot.InUse < 0) || (slot.InUse > slotsize)) throw new ApplicationException($"DEBUG::ParkVehicle - InUse Integrigy test failed - (SlitId: {slot.Id} InUse={slot.InUse})");
+			}
+			int slotcount = garageslots.Count();
+			int slotsizeRequired = vehicle.VehicleType.Size;    // size of vechicle
+			int slotsizeAccumulated = 0;
+			var SlotsAccumulated = new List<Slot>();
+			int counter = 0;
+			while (slotsizeAccumulated < slotsizeRequired) {
+				if (counter == slotcount) break;
+				if (slotsizeRequired >= slotsize) {
+					// Vehicle need at least a full slot
+					if (garageslots[counter].InUse != 0) {
+						counter++;
+						slotsizeAccumulated = 0;
+						if (SlotsAccumulated.Count > 0) SlotsAccumulated.Clear();
+					}
+					else {
+						slotsizeAccumulated += slotsize;
+						SlotsAccumulated.Add(garageslots[counter]);
+						counter++;
+					}
+				}
+				else {
+					// Vehicle do not need a full slot
+					if ((garageslots[counter].InUse < slotsize) && (slotsizeRequired <= (slotsize - garageslots[counter].InUse))) {
+						slotsizeAccumulated = slotsizeRequired;
+						SlotsAccumulated.Add(garageslots[counter]);
+						break;
+					}
+					counter++;
+				}
+			}
 
-
-
-			//			if (pv == null) return false;
-			//			if (pv.State != (int)VehicleState.UnParked) return false;
-
-			// ADD LOGIC TO GET PARKING SLOTS
-
-			//vehicle.slots.Add(slots)
-			//slot.add(vehicle)
-
-			//VehicleSlot
-
-
-			//			pv.State = (int)VehicleState.Parked;
-			//			_context.Vehicles.Update(pv);
-
-			return true;
+			if (slotsizeRequired <= slotsizeAccumulated) {
+				// Managed to get enough slots to park
+				vehicle.CheckInTime = DateTime.Now;
+				foreach (var slot in SlotsAccumulated) {
+					slot.Vehicles.Add(vehicle);
+					if (slotsizeRequired <= slotsize)
+					{
+						slot.InUse += slotsizeRequired; // a fraction of the slot is used
+						slotsizeRequired = 0;
+					}
+					else {
+						slot.InUse = slotsize;          // a full slot is used
+						slotsizeRequired -= slotsize;
+					}
+				}
+				if (slotsizeRequired != 0) throw new ApplicationException("DEBUG::Park - Slot Integrity check failed - left over slots");
+				_context.UpdateRange(SlotsAccumulated);
+				try
+				{
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateException){
+					return false;
+				}
+				return true;
+			}
+			return false;
 		}
 
 		private async Task<bool> UnParkVehicle(int vehicleid)
 		{
 			if (vehicleid <= 0) return false;
-			var vehicle = await _context.Vehicles.FindAsync(vehicleid);
-			if (vehicle == null) return false;
+			var vehicle = _context.Vehicles.Include(v => v.Slots).FirstOrDefault(v => v.Id == vehicleid);
+			if ((vehicle == null) || (vehicle.State != VehicleState.Parked)) return false;
 
-			//			if (vehicle.State != (int)VehicleState.Parked) return;
 
 			// ADD LOGIC TO FREE PARKING SLOTS
 
-			//			vehicle.State = (int)VehicleState.UnParked;
+			vehicle.State = (int)VehicleState.UnParked;
+
 			return true;
 		}
 	}
