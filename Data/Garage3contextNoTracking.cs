95bae41ece2b05dcdf8e3d133b52cfceb34428c8
tom-sharp <dink521g@gmail.com>
2021-09-09 10:46:14 +0200

    Added OverVeiw Controller

---
+++ Data/Garage3contextNoTracking.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3.Data
+{
+	public class Garage3contextNoTracking : Garage3Context
+	{
+
+		public Garage3contextNoTracking(DbContextOptions<Garage3Context> options) : base(options)
+		{
+			base.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+		}
+
+	}
+}
