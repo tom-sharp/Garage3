4a8f595bd8b004949d7cfa2fd5b7daa8ebc0befe
Brosa-SWE <per.gref@gmail.com>
2021-09-11 12:33:49 +0200

    Fixed unique PersonNr validation bug

---
+++ Models/ViewModels/CheckOutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.ViewModels
+{
+	public class CheckOutViewModel
+	{
+		public string LicensePlate { get; set; }
+		public string CheckoutTime { get; set; }
+		public string CheckinTime { get; set; }
+		public string ParkTime { get; set; }
+		public string Amount { get; set; }
+	}
+}
