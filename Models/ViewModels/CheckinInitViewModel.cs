c889472007e61daf14599fb33da54d8c9acf1f0c
rosy871 <yalameju@gmail.com>
2021-09-14 06:02:13 +0200

    email_val

--- Models/ViewModels/CheckinInitViewModel.cs
+++ Models/ViewModels/CheckinInitViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Garage3.Models.ViewModels
     public class CheckinInitViewModel
     {
         public int id { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string PersonEmail { get; set; }
+
     }
 }
